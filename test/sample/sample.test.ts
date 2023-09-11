e00d6a6dc2e0012862f4b74c2775f023944fd893
Christian Dietrich <christian.dietrich.opensource@gmail.com>
2023-09-11 17:42:59 +0200

    initial

---
+++ test/sample/sample.test.ts
@@ -0,0 +1,29 @@
+describe("viech", () => {
+  const x = `
+  demo
+  demo
+  demo`
+  it("dully", () => {
+    console.log(
+      `multi
+    line
+    string` + x
+    )
+    const a =
+      1 + // x
+      1
+    expect(a).toBeGreaterThan(0)
+  })
+  it("dully2", () => {
+    const a = 1
+    expect(a).toBeGreaterThan(0)
+  })
+  it("dully3", () => {
+    const a = 1
+    expect(a).toBeGreaterThan(0)
+  })
+  it("dully4", () => {
+    const a = 1
+    expect(a).toBeGreaterThan(0)
+  })
+})
